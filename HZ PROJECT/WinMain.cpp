b5c82528d3c22c6dcd657470de44ae24a9547a63
fatbahad@gmail.com <fatbahad@gmail.com>
2024-05-28 17:46:11 +0300

    test kbd and mouse input

--- HZ PROJECT/WinMain.cpp
+++ HZ PROJECT/WinMain.cpp
@@ -1,5 +1,7 @@
 #include <windows.h>
 #include <tchar.h> //  содержит макросы для работы с символами и строками в зависимости от настроек Unicode или ANSI приложения
+#include <string>
+#include <sstream>
 
 
 // Определение статических строковых переменных с использованием макроса _T для поддержки обоих ANSI и Unicode символов.
@@ -12,10 +14,51 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
 	switch (msg)
 	{
+
 	case WM_CLOSE:
+	{
 		PostQuitMessage(666);
 		break;
 	}
+
+	case WM_KEYDOWN:
+	{
+		if (wParam == 'F')
+		{
+			SetWindowText(hWnd, _T("KURWA"));
+		}
+		break;
+	}
+
+	case WM_KEYUP:
+	{
+		if (wParam == 'F')
+		{
+			SetWindowText(hWnd, _T("MATKA"));
+		}
+		break;
+	}
+
+	case WM_CHAR:
+	{
+		static std::wstring nString;
+		nString.push_back(wchar_t(wParam));
+		SetWindowText(hWnd, nString.c_str());
+		break;
+	}
+
+	case WM_LBUTTONDOWN:
+	{
+		POINTS pt = MAKEPOINTS(lParam);
+		std::wostringstream woss;
+		woss << "X - " << pt.x << " Y - " << pt.y;
+		SetWindowText(hWnd, woss.str().c_str());
+		break;
+	}
+
+	}
+
+	
 
 	return DefWindowProc(hWnd, msg, wParam, lParam); // DefWindowProc обрабатывает остальные сообщения по умолчанию.
 }
@@ -71,5 +114,5 @@ int WINAPI WinMain(
 		DispatchMessage(&msg);
 	}
 
-	return msg.wParam;
+	return (int)msg.wParam;
 };
