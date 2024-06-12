4709cd196cf9d13683a4c59d67d4f165fa5da6ae
fatbahad@gmail.com <fatbahad@gmail.com>
2024-06-12 13:22:41 +0300

    add comments to Drawable class

--- HZ PROJECT/Drawable.cpp
+++ HZ PROJECT/Drawable.cpp
@@ -12,13 +12,23 @@ void Drawable::Draw(Graphics& gfx)
 
 void Drawable::AddBind(std::unique_ptr<Bindable> bind) 
 {
-	assert("*Must* use AddIndexBuffer to bind index buffer" && typeid(*bind) != typeid(IndexBuffer));
-	binds.push_back(std::move(bind));
+    // Проверка утверждения: нельзя добавлять индексный буфер с помощью AddBind, для этого есть AddIndexBuffer.
+    // Если тип передаваемого объекта - IndexBuffer, выбрасывается ошибка.
+    assert("*Must* use AddIndexBuffer to bind index buffer" && typeid(*bind) != typeid(IndexBuffer));
+
+    // Перемещение уникального указателя bind в контейнер binds.
+    binds.push_back(std::move(bind));
 }
 
-void Drawable::AddIndexBuffer(std::unique_ptr<IndexBuffer> ibuf) 
+void Drawable::AddIndexBuffer(std::unique_ptr<IndexBuffer> ibuf)
 {
-	assert("Attempting to add index buffer a second time" && pIndexBuffer == nullptr);
-	pIndexBuffer = ibuf.get();
-	binds.push_back(std::move(ibuf));
+    // Проверка утверждения: нельзя добавлять индексный буфер больше одного раза.
+    // Если индексный буфер уже установлен (pIndexBuffer не равен nullptr), выбрасывается ошибка.
+    assert("Attempting to add index buffer a second time" && pIndexBuffer == nullptr);
+
+    // Сохранение указателя на индексный буфер в члене pIndexBuffer.
+    pIndexBuffer = ibuf.get();
+
+    // Перемещение уникального указателя ibuf в контейнер binds.
+    binds.push_back(std::move(ibuf));
 }
